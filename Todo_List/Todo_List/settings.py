ee72ad5e739c233aac046f833b90aa6b837e995d
SILVER KING <106859103+NAHIAN-19@users.noreply.github.com>
2023-09-28 00:38:01 +0600

    Update settings.py

--- Todo_List/Todo_List/settings.py
+++ Todo_List/Todo_List/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Todo_List.urls'
