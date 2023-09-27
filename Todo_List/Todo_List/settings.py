1a87e436c7fe5b19e661b42d1a9cbfd21a489af1
SILVER KING <106859103+NAHIAN-19@users.noreply.github.com>
2023-09-28 00:41:06 +0600

    Update settings.py

--- Todo_List/Todo_List/settings.py
+++ Todo_List/Todo_List/settings.py
@@ -121,7 +121,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
