6569db1063d84d71ddd45b385c98156652b8c705
SILVER KING <106859103+NAHIAN-19@users.noreply.github.com>
2023-10-19 20:24:55 +0600

    Update forms.py

--- Todo_List/Todo_List_App/forms.py
+++ Todo_List/Todo_List_App/forms.py
@@ -1,51 +1,71 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm, SetPasswordForm
-from django.contrib.auth.models import User
-from .models import Profile
-from django.core.exceptions import ValidationError
-from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, SetPasswordForm, UserChangeForm
+from django.contrib.auth import get_user_model
+from .models import Profile, CustomUser
 from django.contrib.auth.models import User
 
+User = get_user_model()
+
+class CustomUserAdminForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
 class SignInForm(AuthenticationForm):
     pass
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    phone = forms.CharField(max_length=15, required=False, help_text='Optional.')
+    address = forms.CharField(max_length=100, required=False, help_text='Optional.')
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'phone', 'address', 'password1', 'password2']
+
     def clean_username(self):
         username = self.cleaned_data['username']
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError('This username is already in use. Please choose a different one.')
         return username
+
     def clean_email(self):
         email = self.cleaned_data['email']
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('This email address is already associated with an account.')
         return email
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get('password1')
-        password2 = cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords do not match.")
-        return cleaned_data
 
 class ProfileForm(forms.ModelForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'autocomplete': 'off'}))
+    first_name = forms.CharField(max_length=30, required=False, label="First Name")
+    last_name = forms.CharField(max_length=30, required=False, label="Last Name")
+    phone = forms.CharField(max_length=15, required=False)
+    address = forms.CharField(max_length=100, required=False)
 
     class Meta:
         model = Profile
-        fields = ['email', 'profile_picture']
+        fields = ['email','phone','address', 'profile_picture']
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = self.instance.user
 
-    def __init__(self, *args, **kwargs):
-        super(ProfileForm, self).__init__(*args, **kwargs)
-        self.fields['email'].initial = self.instance.user.email
+        if User.objects.exclude(pk=user.pk).filter(email=email).exists():
+            raise forms.ValidationError('This email address is already associated with an account.')
 
-    def save(self, commit=True):
-        self.instance.user.email = self.cleaned_data['email']
-        return super(ProfileForm, self).save(commit=commit)
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name and last_name:
+            full_name = f"{first_name} {last_name}"
+            self.instance.full_name = full_name
+
+        return cleaned_data
 
 class CustomSetPasswordForm(SetPasswordForm):
     new_password1 = forms.CharField(label="New Password", widget=forms.PasswordInput)
@@ -60,8 +80,7 @@ class CustomSetPasswordForm(SetPasswordForm):
             raise forms.ValidationError("Passwords do not match.")
         
         return cleaned_data
-    
+
 class SolveForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
-
